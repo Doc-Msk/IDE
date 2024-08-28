bd2e69443ad47ca2ebc2889915060d1f68aedb08
Artem <gub.art.al@gmail.com>
2024-08-28 22:15:53 +0300

    commit

---
+++ functions/outliners_z_score.py
@@ -0,0 +1,12 @@
+def outliers_z_score_mode(data, feature, left = 3, right = 3, log_scale=False):
+    if log_scale:
+        x = np.log(data[feature]+1)
+    else:
+        x = data[feature]
+    mu = x.mean()
+    sigma = x.std()
+    lower_bound = mu - left * sigma
+    upper_bound = mu + right * sigma
+    outliers = data[(x < lower_bound) | (x > upper_bound)]
+    cleaned = data[(x >= lower_bound) & (x <= upper_bound)]
+    return outliers, cleaned
